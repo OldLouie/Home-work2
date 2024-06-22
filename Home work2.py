5351a8e24911f32c45012573d7960c2c2d36f0fe
Egor <repingign@gmail.com>
2024-06-22 23:53:34 +0300

    Add files via upload

---
+++ Home work2.py
@@ -0,0 +1,6 @@
+Total_tasks = '12'
+Hours_spen = '1.5'
+Course = 'Pyhton'
+Average_execution_time = float(Hours_spen) / int(Total_tasks)
+print('Курс' + ':', Course + ',', 'всего задач' + ':', Total_tasks + ',', 'затрачено часов' ': ',end='')
+print(Hours_spen, ", среднее время выполнение:", Average_execution_time)
